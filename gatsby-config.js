b1f6d362599bd95c4053588b11e038ab7edac33b
parkeunsik <qkrdmstlr3@naver.com>
2021-10-09 01:57:38 +0900

    [chore] init Gatsby
    - initialize with `npm init gatsby`

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "techstack-generator",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
