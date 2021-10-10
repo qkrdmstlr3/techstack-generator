d8aa5ecc72425f3022ff41503de0cff832da8ce5
parkeunsik <qkrdmstlr3@naver.com>
2021-10-10 16:48:02 +0900

    [fix] reinstall storybook with webpack5 version
    - There was a conflict between gatsby v3 and the existing storybook, allowing the storybook to build using webpack5.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs',
-    '@storybook/addon-actions',
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-};
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "core": {
+    "builder": "webpack5"
+  }
+}
