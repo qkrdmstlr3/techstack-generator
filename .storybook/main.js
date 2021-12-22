b347b473c671f923260fc55c7007eb6f53d9e59e
parkeunsik <qkrdmstlr3@naver.com>
2021-12-22 21:23:14 +0900

    [feat] install storybook-knobs
    - To figure out props, install knobs library

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-actions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+  ],
   core: {
     builder: 'webpack5',
   },
