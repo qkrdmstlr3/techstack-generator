87f3c543191d2cb5090cf2fef08da683b02232b1
parkeunsik <qkrdmstlr3@naver.com>
2021-12-22 21:25:20 +0900

    [refactor] add knob addon to ResultTemplate.stories
    - add globalStyle to preview.js
    - add addon to ResultTemplate story file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,20 @@
+import GlobalStyle from '../src/style/GlobalStyle';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Story />
+      <GlobalStyle />
+    </>
+  ),
+];
--- src/components/templates/result/ResultTemplate.stories.tsx
+++ src/components/templates/result/ResultTemplate.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withKnobs, object } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import ResultTemplate from '.';
 import { SettingType } from '../setting/index';
@@ -8,10 +9,11 @@ import { TechType } from '../select/index';
 export default {
   title: 'Templates/resultTemplate',
   component: ResultTemplate,
+  decorators: [withKnobs],
 };
 
 const techs: TechType[] = new Array(20).fill(0).map((_, index) => ({
-  src: 'https://github.com/qkrdmstlr3/techstack-generator/raw/master/techs/js-icon/js-icon.gif',
+  src: 'js-icon.svg',
   selected: true,
   number: index + 1,
 }));
@@ -23,9 +25,10 @@ const setting: SettingType = {
   results: [ResultType.html],
 };
 
+const techsKnob = object('techs', techs);
+const settingKnob = object('setting', setting);
 const changeAction = action('onchange');
+
 export const resultTemplate = () => (
-  <div style={{ backgroundColor: 'black' }}>
-    <ResultTemplate setting={setting} techs={techs} changeTemplate={changeAction} />
-  </div>
+  <ResultTemplate setting={settingKnob} techs={techsKnob} changeTemplate={changeAction} />
 );
