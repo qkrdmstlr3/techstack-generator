e7e029aa9dca96b32857b77ee453e82d3ab9396d
parkeunsik <qkrdmstlr3@naver.com>
2021-12-22 21:35:52 +0900

    [refactor] add knob addon to SelectTemplate.stories
    - add addon to SelectTemplate story file

--- src/components/templates/select/SelectTemplate.stories.tsx
+++ src/components/templates/select/SelectTemplate.stories.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
+import { withKnobs, object } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import SelectTemplate, { TechType } from '.';
 
 export default {
   title: 'Templates/selectTemplate',
   component: SelectTemplate,
+  decorators: [withKnobs],
 };
 
 const techs: TechType[] = new Array(20).fill(0).map((_, index) => ({
-  src: String(index),
+  src: 'https://techstack-generator.vercel.app/js-icon.svg',
   selected: false,
-  number: 0,
+  number: index + 1,
 }));
 
+const techsKnob = object('techs', techs);
 const clickAction = action('onclick');
 const changeAction = action('onchange');
+
 export const selectTemplate = () => (
-  <div style={{ backgroundColor: 'black' }}>
-    <SelectTemplate techs={techs} selectTech={clickAction} changeTemplate={changeAction} />
-  </div>
+  <SelectTemplate techs={techsKnob} selectTech={clickAction} changeTemplate={changeAction} />
 );
