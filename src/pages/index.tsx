5f7118096079bc50286029f10ef779d663e0b081
isaac_lee <yy0221ss@gmail.com>
2022-05-07 02:59:24 +0900

    [feat] add python-icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,6 +29,7 @@ const techsSrc: string[] = [
   'prettier-icon.svg',
   'jest-icon.svg',
   'testinglibrary-icon.svg',
+  'python-icon.svg',
   'django-icon.svg',
   'graphql-icon.svg',
   'restapi-icon.svg',
